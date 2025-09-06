dbfb63c21c8cedbb78b06e3b2f847912607ff139
zarious-dev <enriquez.caezarie@gmail.com>
2025-09-06 14:32:46 +0800

    Add essential features activity

---
+++ 11_javascript-refresher/08_essential_features.js
@@ -0,0 +1,20 @@
+//array of hobbies 
+const hobbies = ["reading", "watching", "listening to music"]
+hobbies.map(hobby => console.log(hobby))
+
+//destructuring 
+const student = {
+    name: "Rad", age: 15
+}
+const {name, age} = student
+console.log(name, age)
+
+
+const array = [1, 2, 3]
+const newArray = [...array, 4, 5] 
+console.log(newArray) 
+
+
+
+
+
