41fcfab457502e8417c6a668a3a70eab0d1ffd03
zarious-dev <enriquez.caezarie@gmail.com>
2025-09-06 14:28:02 +0800

    Add functions activity

---
+++ 11_javascript-refresher/03_functions.js
@@ -0,0 +1,19 @@
+//greet function
+function greet (name) {
+    return `Hello, ${name}!`
+}
+
+console.log(greet("Caezarie"))
+
+
+//arrow function
+const square = num => num * num 
+    console.log(square(11))
+
+
+//calculator function 
+function calculator(a,b) {
+    console.log("sum:", a + b, "product:", a * b)
+}
+
+calculator(2, 11);
