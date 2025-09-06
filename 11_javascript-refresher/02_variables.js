3f1b02c8e11ae8b5fe7f70572e1f7ad59c263503
zarious-dev <enriquez.caezarie@gmail.com>
2025-09-06 14:26:55 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,21 @@
+let name = "Caezarie"
+let age = 15
+let isStudent = true
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isStudent)
+
+//arithmetic operators
+let a = 11
+let z = 8
+console.log(a + z)
+console.log(a - z)
+console.log(a * z)
+console.log(a / z)
+
+//comparison operators
+console.log("5" == 5) 
+console.log("5" === 5) 
+
+
