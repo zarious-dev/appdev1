e6fd0c86cb03efd96fff0a8be80ee22630ded712
zarious-dev <enriquez.caezarie@gmail.com>
2025-09-06 14:31:46 +0800

    Add control structures activity

---
+++ 11_javascript-refresher/06_control_structures.js
@@ -0,0 +1,26 @@
+// if-else statement
+let grade = 70
+if (grade >= 90) {
+    console.log("A")
+}   
+else if (grade >= 80) {
+    console.log("B")
+}
+else if (grade >= 70) {
+    console.log("C")
+}
+else {
+    console.log("F")
+}
+
+//for loop
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+
+//while loop
+let count = 0
+while (count < 3) {
+    console.log("Hello")
+    count++
+}
