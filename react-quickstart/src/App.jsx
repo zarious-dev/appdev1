c1f04138a3a1769e3828e64e68db2aef2c5bd2e1
zarious-dev <enriquez.caezarie@gmail.com>
2025-09-13 14:37:10 +0800

    Done creating and nesting components

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function MyButton() {  
+  return (
+  <button>Click Me!</button>
+  )
+}
+
+function App() {
+  return (
+    <>
+    <h1>Hello, World!</h1>
+    <MyButton />    
+    </>
+  )
+}
+
+export default App
