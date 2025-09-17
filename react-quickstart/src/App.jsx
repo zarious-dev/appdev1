fd7a22b385470dcb710da4e2c4bccb7ee662c724
zarious-dev <enriquez.caezarie@gmail.com>
2025-09-17 16:15:33 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
 
-function AboutPage() {
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+}
+
+export default function Profile() {
   return (
     <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-      <img className='avatar' style={{ borderRadius: '50%' }}/>
+      <h1>{user.name}</h1>
+      <img
+        className='avatar'
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
     </>
   )
 }
-
-export default AboutPage
