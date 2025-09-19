3cfbc9b11a44cdd43bce7facaec2ceac78e887ae
zarious-dev <enriquez.caezarie@gmail.com>
2025-09-20 02:35:45 +0800

    Done sharing data between components

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,70 +1,67 @@
 import './App.css'
 import { useState } from 'react'
 
-//responding to events
-function MyButton() {
-  function handleClick() {
-    alert('You clicked me!')
-  }
-
+// sharing data between components
+function MyButton({ count, onClick }) {
   return (
-    <button onClick={handleClick}>
-      Click me
-    </button>
-  )
-}
-
-
-//updating the screen / using hooks
-function MyButton1() {
-  const [count, setCount] = useState(0)
-
-  function handleClick() {
-    setCount(count + 1)
-  }
-
-  return (
-    <button onClick={handleClick}>
+    <button onClick={onClick}>
       Clicked {count} times
     </button>
   )
 }
 
-function App() {
+export default function MyApp() {
+  const [count, setCount] = useState(0)
+  
+  function handleClick() {
+    setCount(count + 1)
+  }   
+
   return (
-    <>
-      <MyButton />
-      <h1>Counters that update separately</h1>
-      <MyButton1 />
-      <MyButton1 />
-    </>
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick}/>
+      <MyButton count={count} onClick={handleClick}/>
+    </div>
   )
 }
 
-export default App
+// responding to events
+// function MyButton() {
+//   function handleClick() {
+//     alert('You clicked me!')
+//   }
 
-
-// rendering lists
-// const products = [
-//   { title: 'Cabbage', isFruit: false, id: 1 },
-//   { title: 'Garlic', isFruit: false, id: 2 },
-//   { title: 'Apple', isFruit: true, id: 3 },
-// ]
-
-// function ShoppingList() {
-//   const listItems = products.map(product =>
-//     <li
-//       key={product.id}
-//       style={{
-//         color: product.isFruit ? 'magenta' : 'darkgreen'
-//       }}
-//     >
-//       {product.title}
-//     </li>
+//   return (
+//     <button onClick={handleClick}>
+//       Click me
+//     </button>
 //   )
+// }
+
+
+// updating the screen / using hooks
+// function MyButton1() {
+//   const [count, setCount] = useState(0)
+
+//   function handleClick() {
+//     setCount(count + 1)
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Clicked {count} times
+//     </button>
+//   )
+// }
+
+// function App() {
 //   return (
 //     <>
-//       <ul>{listItems}</ul>
+//       <MyButton />
+//       <h1>Counters that update separately</h1>
+//       <MyButton1 />
+//       <MyButton1 />
 //     </>
 //   )
 // }
