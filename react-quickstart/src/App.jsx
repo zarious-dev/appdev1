2270acb7e160acdfaea9cd607a0bfb54502e8653
zarious-dev <enriquez.caezarie@gmail.com>
2025-09-20 02:17:37 +0800

    Done responding to events / using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,29 +1,70 @@
 import './App.css'
+import { useState } from 'react'
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-]
+//responding to events
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!')
+  }
 
-export default function ShoppingList() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  )
   return (
-    <ul>{listItems}</ul>
+    <button onClick={handleClick}>
+      Click me
+    </button>
   )
 }
 
 
+//updating the screen / using hooks
+function MyButton1() {
+  const [count, setCount] = useState(0)
 
-// export default ShoppingList
-// export default App
+  function handleClick() {
+    setCount(count + 1)
+  }
 
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <MyButton />
+      <h1>Counters that update separately</h1>
+      <MyButton1 />
+      <MyButton1 />
+    </>
+  )
+}
+
+export default App
+
+
+// rendering lists
+// const products = [
+//   { title: 'Cabbage', isFruit: false, id: 1 },
+//   { title: 'Garlic', isFruit: false, id: 2 },
+//   { title: 'Apple', isFruit: true, id: 3 },
+// ]
+
+// function ShoppingList() {
+//   const listItems = products.map(product =>
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? 'magenta' : 'darkgreen'
+//       }}
+//     >
+//       {product.title}
+//     </li>
+//   )
+//   return (
+//     <>
+//       <ul>{listItems}</ul>
+//     </>
+//   )
+// }
