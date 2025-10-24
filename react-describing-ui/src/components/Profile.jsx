11a2ebc06ca184233eb1409c8c9e9b0d62f387b5
zarious-dev <enriquez.caezarie@gmail.com>
2025-10-24 22:01:12 +0800

    Done with when and how to create multi-component files

--- react-describing-ui/src/components/Profile.jsx
+++ react-describing-ui/src/components/Profile.jsx
@@ -1,8 +1,8 @@
 export default function Profile() {
   return (
     <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      alt="Alan L. Hart"
     />
   );
 }
