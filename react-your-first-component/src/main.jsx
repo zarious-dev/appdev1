7bf44a8d74cdbe6e90d42fbe9396fc23158c2ec4
zarious-dev <enriquez.caezarie@gmail.com>
2025-11-09 15:44:35 +0800

    Done with using a component

--- react-your-first-component/src/main.jsx
+++ react-your-first-component/src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
-import Profile from './components/Profile.jsx'
-
+import Gallery from './components/Gallery.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Profile />
+    <Gallery />
   </StrictMode>,
 )
