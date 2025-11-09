086b72e086b96c8cd0e9f541858e5b543fdea312
zarious-dev <enriquez.caezarie@gmail.com>
2025-11-09 17:25:28 +0800

    Done with exporting and importing a component

--- react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/src/App.jsx
@@ -1,19 +1,7 @@
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
+import Gallery from './components/Gallery.jsx';
 
-export default function Gallery() {
+export default function App() {
   return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <Gallery />
   );
 }
