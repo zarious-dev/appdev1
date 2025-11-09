fb422e2213f09896b0b7af240379d6388af76bbb
zarious-dev <enriquez.caezarie@gmail.com>
2025-11-09 19:36:29 +0800

    Done with the rules of JSX

--- react-writing-markup-with-jsx/src/App.jsx
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -1,19 +1,23 @@
-//this will not work because there are rules to follow 
+//this is converted and followed the rules of JSX 
 
 export default function TodoList() {
   return (
-    //missing opening fragment tag <> or <div>
-    <h1>Hedy Lamarr's Todos</h1>
-    <img 
-      src="https://i.imgur.com/yXOvdOSs.jpg" 
-      alt="Hedy Lamarr" 
-      class="photo" //class should be className
-    > //missing closing tag
-    <ul>
-      <li>Invent new traffic lights        //missing closing </li> tag
-      <li>Rehearse a movie scene           //missing closing </li> tag
-      <li>Improve the spectrum technology  //missing closing </li> tag
-    </ul>
-    //missing closing fragment tag </> or </div>
+    <> 
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li> 
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </> 
   );
 }
+
+//wrap in a parent tag (fragment or div)
+//close img and li tags
+//use camelCase for className 
