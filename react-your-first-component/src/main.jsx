6955e3ea10c11bec0c5a1d84502ef19cec9b0fae
zarious-dev <enriquez.caezarie@gmail.com>
2025-11-09 15:29:23 +0800

    Done with defining a component

--- react-your-first-component/src/main.jsx
+++ react-your-first-component/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import Profile from './components/Profile.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Profile />
   </StrictMode>,
 )
