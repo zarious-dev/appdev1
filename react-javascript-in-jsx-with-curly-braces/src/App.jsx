338ed1afe851a60ade0f5abf6a335b090e2916cc
zarious-dev <enriquez.caezarie@gmail.com>
2025-11-22 14:40:15 +0800

    Done with passing strings with quote

---
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+export default function App() {
+  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const description = 'Gregorio Y. Zara';
+  return (
+    <img
+      className="avatar"
+      src={avatar}
+      alt={description}
+    />
+  );
+}
+
+
+ 
