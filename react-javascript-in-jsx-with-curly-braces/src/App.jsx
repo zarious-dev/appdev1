8fc21f2f3bc328fe4dd767a945448d1a2342bc2a
zarious-dev <enriquez.caezarie@gmail.com>
2025-11-22 14:48:44 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,16 +1,9 @@
 import './App.css'
 
 export default function App() {
-  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
-  const description = 'Gregorio Y. Zara';
+  const name = 'Gregorio Y. Zara';
   return (
-    <img
-      className="avatar"
-      src={avatar}
-      alt={description}
-    />
+    <h1>{name}'s To Do List</h1>
+    
   );
 }
-
-
- 
