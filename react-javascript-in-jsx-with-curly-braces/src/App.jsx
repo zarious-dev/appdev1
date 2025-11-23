fa4e7a019bbc53f4c64dfce35e39f12b703c27fc
zarious-dev <enriquez.caezarie@gmail.com>
2025-11-23 21:56:44 +0800

    Done with more fun with JavaScript objects and curly braces

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,14 +1,27 @@
-import './App.css'
+import './App.css';
+
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
 
 export default function TodoList() {
   return (
-    <ul style={{
-      backgroundColor: 'black',
-      color: 'pink'
-    }}>
-      <li>Improve the videophone</li>
-      <li>Prepare aeronautics lectures</li>
-      <li>Work on the alcohol-fuelled engine</li>
-    </ul>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
   );
 }
