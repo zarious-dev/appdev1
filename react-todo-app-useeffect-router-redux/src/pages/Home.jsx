857c0be0743fc1ca438dbe320f3b79c7f37ae1bc
zarious-dev <enriquez.caezarie@gmail.com>
2025-11-28 21:43:36 +0800

    Added style to Home page

--- react-todo-app-useeffect-router-redux/src/pages/Home.jsx
+++ react-todo-app-useeffect-router-redux/src/pages/Home.jsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <div>
+    <div
+      style={{
+        padding: "40px",
+        textAlign: "center"
+      }}
+    >
       <h1>Welcome to the Todo App</h1>
       <Link to="/login">Go to Login</Link>
     </div>
