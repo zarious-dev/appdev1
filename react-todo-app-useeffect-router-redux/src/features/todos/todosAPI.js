e26e2e9a5f7abc50f3fbc51a9800bd861fa3659b
zarious-dev <enriquez.caezarie@gmail.com>
2025-11-28 19:47:04 +0800

    Done with Todo API helpers section

---
+++ react-todo-app-useeffect-router-redux/src/features/todos/todosAPI.js
@@ -0,0 +1,29 @@
+export const getTodosAPI = function () {
+    return fetch(`https://jsonplaceholder.typicode.com/todos?_limit=10`)
+    .then(res => res.json())
+}
+
+export const addTodoAPI = function (todo) {
+    return fetch(`https://jsonplaceholder.typicode.com/todos`, {
+        method: "POST",
+        body: JSON.stringify(todo),
+        headers: { "Content-Type": "application/json" }
+    })
+    .then(res => res.json())
+}
+
+export const updateTodoAPI = function(todo) {
+    return fetch(`https://jsonplaceholder.typicode.com/todos/${todo.id}`, {
+        method: "PUT",
+        body: JSON.stringify(todo),
+        headers: { "Content-Type": "application/json" }
+    })
+    .then(res => res.json())
+}
+
+export const deleteTodoAPI = function(id) {
+    return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: "DELETE"
+    })
+    .then(() => id)
+}
